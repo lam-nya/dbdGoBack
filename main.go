e133ff8e263d16adf44f0282699bf1dc3e104a0d
root <root@lamnya.fvds.ru>
2023-04-29 23:46:10 +0300

    Implement base webserver.

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+  "net/http"
+  "github.com/gin-gonic/gin"
+)
+
+func main() {
+  router := gin.Default()
+
+  router.GET("/", func(context *gin.Context) {
+    context.JSON(http.StatusOK, gin.H{"data": "hello world"})
+  })
+
+  router.GET("/auth", func(context *gin.Context) {
+    context.JSON(200, gin.H{"username": "username1"})
+  })
+
+  err := router.Run(":81")
+  if err != nil {
+    panic("[Error] failled to start GIN:"+err.Error())
+    return
+  }
+}
